3b7939b655beaf04fe5312ce159fc06e5fc6c776
Eiji <64912886+Eiji127@users.noreply.github.com>
2021-03-16 23:30:31 +0900

    [Upload]:add ABC049B.swift...

---
+++ ABC/ABC049/ABC049B.swift
@@ -0,0 +1,21 @@
+
+func readTwoInts() -> (a: Int, b: Int) {
+    let ints = readLine()!.split(separator: " ").map { Int($0)! }
+    return (a: ints[0], b: ints[1])
+}
+
+func main() {
+    let (h, w) = readTwoInts()
+    var pixels = [String]()
+    for i in 0..<h {
+        let pixel = readLine()!
+        pixels.append(pixel)
+    }
+    for j in 0..<h {
+        print(pixels[j])
+        print(pixels[j])
+    }
+}
+ 
+main()
+
